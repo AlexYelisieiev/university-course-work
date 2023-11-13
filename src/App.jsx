44c56630a09aab215d8e3d5a468d3a25abc3eda4
Alex <taus.anarchy@gmail.com>
2023-11-13 14:22:37 +0200

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,19 @@ import AchievementSection from './components/AchievementSection';
 const App = () => {
     return (
         <div className='container'>
-            <h1>Gamiee</h1>
+            <nav className="navbar navbar-expand-sm">
+                <a href="/" className="navbar-logo">Gamiee</a>
+                <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className="collapse navbar-collapse" id='navbarContent'>
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><a href="/" className="nav-link">Nickname</a></li>
+                        <li className="nav-item"><a href="/" className="nav-link">Bio</a></li>
+                        <li className="nav-item"><a href="/" className="nav-link">Stats</a></li>
+                    </ul>
+                </div>
+            </nav>
             <NicknameGenerator />
             <BioGenerator />
             <AchievementSection />
