89c05af4daaec0046f3201410385cf4ea4a69c73
Alex <taus.anarchy@gmail.com>
2023-11-14 15:16:40 +0200

    Fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,38 @@ import BioGenerator from './components/BioGenerator';
 import AchievementSection from './components/AchievementSection';
 
 const App = () => {
-    return (
-        <div className="conainer-fluid">
-            <nav className="navbar navbar-expand-sm px-1">
-                <a href="/" className="navbar-brand">Gamiee</a>
-                <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-                    <span className='navbar-toggler-icon'></span>
-                </button>
-                <div className="collapse navbar-collapse" id='navbarContent'>
-                    <ul className="navbar-nav">
-                        <li className="nav-item"><a href="/" className="nav-link">Nickname</a></li>
-                        <li className="nav-item"><a href="/" className="nav-link">Bio</a></li>
-                        <li className="nav-item"><a href="/" className="nav-link">Stats</a></li>
-                    </ul>
-                </div>
-            </nav>
-            <div className='container'>
-                <div className="row">
-                    <NicknameGenerator/>
-                </div>
-                <div className="row">
-                    <BioGenerator/>
-                </div>
-                <div className="row">
-                    <AchievementSection/>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<nav className="navbar navbar-expand-sm px-1">
+				<div className="container-fluid">
+					<a href="/" className="navbar-brand">Gamiee</a>
+					<button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+						<span className='navbar-toggler-icon'></span>
+					</button>
+					<div className="collapse navbar-collapse" id='navbarContent'>
+						<ul className="navbar-nav">
+							<li className="nav-item"><a href="/" className="nav-link">Nickname</a></li>
+							<li className="nav-item"><a href="/" className="nav-link">Bio</a></li>
+							<li className="nav-item"><a href="/" className="nav-link">Stats</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div className="conainer-fluid">
+				<div className='container'>
+					<div className="row">
+						<NicknameGenerator />
+					</div>
+					<div className="row">
+						<BioGenerator />
+					</div>
+					<div className="row">
+						<AchievementSection />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
