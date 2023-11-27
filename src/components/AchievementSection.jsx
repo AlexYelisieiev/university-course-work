cff1f022d5b84eb3fd2dff6189da9178382ab35c
Alex <taus.anarchy@gmail.com>
2023-11-27 17:36:58 +0200

    fix

--- src/components/AchievementSection.jsx
+++ src/components/AchievementSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './AchievementSection.css';
+import styles from './AchievementSection.css';
 
 const AchievementSection = () => {
   const [game, setGame] = useState('');
