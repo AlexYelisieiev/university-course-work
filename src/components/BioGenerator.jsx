b53cc128433907e3e6c14e30f21ed087b417e441
Alex <taus.anarchy@gmail.com>
2023-12-05 16:13:17 +0200

    Add comments

--- src/components/BioGenerator.jsx
+++ src/components/BioGenerator.jsx
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
 
 const BioGenerator = () => {
+    // State for the currently selected theme for bio generation
     const [selectedTheme, setSelectedTheme] = useState('');
+
+    // State for the dynamic sections of the bio, initialized with some defaults
     const [sections, setSections] = useState([
         { label: 'Profession', value: '' },
         { label: 'Hobbies', value: '' },
         { label: 'Catchphrase', value: '' },
     ]);
+
+    // State for the generated bio text
     const [bio, setBio] = useState('');
 
+    // Function to update the value of a specific section
     const updateSectionValue = (index, value) => {
         const updatedSections = [...sections];
         updatedSections[index].value = value;
         setSections(updatedSections);
     };
 
+    // Function to update the label of a specific section
     const updateSectionLabel = (index, label) => {
         const updatedSections = [...sections];
         updatedSections[index].label = label;
         setSections(updatedSections);
     };
 
+    // Function to add a new default section
     const addSection = (event) => {
         event.preventDefault();
         const newSection = { label: 'New Section', value: '' };
         setSections([...sections, newSection]);
     };
 
+    // Function to remove a section at a given index
     const removeSection = (event, index) => {
         event.preventDefault();
         const updatedSections = [...sections];
@@ -55,9 +64,12 @@ const BioGenerator = () => {
         <div className='px-3 py-2'>
             <h2 className='mb-2' style={{ paddingTop: "7.5vmin" }}>Bio generator</h2>
             <form>
+                {/* Theme selection row */}
                 <div className="row mb-2">
+                    {/* Label for the theme selection dropdown */}
                     <label className='col-sm-3 col-form-label' htmlFor="theme">Select Theme:</label>
                     <div className='col-sm-9'>
+                        {/* Dropdown for selecting the bio theme */}
                         <select
                             className='form-select'
                             id="theme"
@@ -65,6 +77,7 @@ const BioGenerator = () => {
                             defaultValue={'default'}
                             onChange={(e) => setSelectedTheme(e.target.value)}
                         >
+                            {/* Dropdown options for theme selection */}
                             <option value="default">Default</option>
                             <option value="sections">Sections</option>
                             <option value="dashes">Dashes</option>
@@ -72,14 +85,15 @@ const BioGenerator = () => {
                             <option value="jungle">Jungle Style</option>
                             <option value="beach">Beach Style</option>
                             <option value="stars">Stars Style</option>
-                            {/* Add more themes as needed */}
                         </select>
                     </div>
                 </div>
 
+                {/* Map through sections to display input fields for section labels and values */}
                 {sections.map((section, index) => (
-                    <div>
-                        <div key={index} className="row mb-2">
+                    <div key={index}>
+                        {/* Input for section title */}
+                        <div className="row mb-2">
                             <label className='col-sm-4 col-form-label' htmlFor={`section-title-${index}`}>Section Title:</label>
                             <div className='col-sm-8'>
                                 <input
@@ -91,6 +105,7 @@ const BioGenerator = () => {
                                 />
                             </div>
                         </div>
+                        {/* Input for section content */}
                         <div className='row mb-2'>
                             <label className='col-sm-4 col-form-label' htmlFor={`section-${index}`}>Section Content:</label>
                             <div className='col-sm-8'>
@@ -102,6 +117,7 @@ const BioGenerator = () => {
                                     onChange={(e) => updateSectionValue(index, e.target.value)}
                                 />
                             </div>
+                            {/* Button to remove a section */}
                             <div>
                                 <button className='btn btn-primary my-2' onClick={event => removeSection(event, index)}>
                                     Remove
@@ -112,14 +128,17 @@ const BioGenerator = () => {
                 ))}
 
                 <div className='mt-2 mb-3'>
+                    {/* Button to add a new section */}
                     <button className='btn btn-primary me-2' onClick={addSection}>
                         Add Section
                     </button>
+                    {/* Button to generate the bio based on the entered sections */}
                     <button className='btn btn-primary' onClick={event => { event.preventDefault(); generateBio(event) }}>
                         Generate Bio
                     </button>
                 </div>
 
+                {/* Display area for the generated bio */}
                 <div className='row'>
                     <span className='text-bold'>Generated Bio:</span>
                     <pre>{bio}</pre>
--- src/components/NicknameGenerator.jsx
+++ src/components/NicknameGenerator.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const NicknameGenerator = () => {
+	// State hooks for various nickname generator options
 	const [name, setName] = useState('');
 	const [nickname, setNickname] = useState('');
 	const [asciiStyle, setAsciiStyle] = useState('');
@@ -9,65 +10,115 @@ const NicknameGenerator = () => {
 	const [useSuffix, setUseSuffix] = useState(true);
 
 	const generateNickname = () => {
-	const prefixes = ['Happy', 'Savage', 'Mystic', 'Epic', 'Legendary', 'Swift', 'Atomic', 'Thunder', 'Inferno', 'Galactic'];
-	const suffixes = ['Gamer', 'Master', 'Ninja', 'Champion', 'Wizard', 'Heroic', 'Eagle', 'Pro', 'Sorcerer', 'Warrior'];
-	const emojis = ['😄', '🔥', '🌟', '🎮', '👾', '🚀', '👑', '💡', '🎲', '🐉', '🌈', '🍀', '🌌', '⚡'];
+		// Predefined lists of prefixes, suffixes, and emojis for generating nicknames
+		const prefixes = ['Happy', 'Savage', 'Mystic', 'Epic', 'Legendary', 'Swift', 'Atomic', 'Thunder', 'Inferno', 'Galactic'];
+		const suffixes = ['Gamer', 'Master', 'Ninja', 'Champion', 'Wizard', 'Heroic', 'Eagle', 'Pro', 'Sorcerer', 'Warrior'];
+		const emojis = ['😄', '🔥', '🌟', '🎮', '👾', '🚀', '👑', '💡', '🎲', '🐉', '🌈', '🍀', '🌌', '⚡'];
 
-	const asciiStyles = {
-		uppercase: (str) => str.toUpperCase(),
-		leet: (str) => str.replace(/[aeio]/g, (match) => ({ 'a': '4', 'e': '3', 'i': '1', 'o': '0' }[match])),
+		// Object containing functions to apply ASCII styles to the nickname
+		const asciiStyles = {
+			uppercase: (str) => str.toUpperCase(),
+			leet: (str) => str.replace(/[aeio]/g, (match) => ({ 'a': '4', 'e': '3', 'i': '1', 'o': '0' }[match])),
+		};
+
+		// Randomly selects a prefix, suffix, and emoji if their respective options are enabled
+		const randomPrefix = usePrefix ? prefixes[Math.floor(Math.random() * prefixes.length)] : '';
+		const randomSuffix = useSuffix ? suffixes[Math.floor(Math.random() * suffixes.length)] : '';
+		const randomEmoji = useEmojis ? emojis[Math.floor(Math.random() * emojis.length)] : '';
+
+		// Constructs the nickname using the random selections and the provided name
+		let generatedNickname = `${randomEmoji} ${randomPrefix} ${name} ${randomSuffix} ${randomEmoji}`;
+
+		// Apply the selected ASCII style if it exists
+		if (asciiStyles[asciiStyle]) {
+			generatedNickname = asciiStyles[asciiStyle](generatedNickname);
+		}
+
+		// Update the state with the newly generated nickname
+		setNickname(generatedNickname);
 	};
 
-	const randomPrefix = usePrefix ? prefixes[Math.floor(Math.random() * prefixes.length)] : '';
-	const randomSuffix = useSuffix ? suffixes[Math.floor(Math.random() * suffixes.length)] : '';
-	const randomEmoji = useEmojis ? emojis[Math.floor(Math.random() * emojis.length)] : '';
-
-	let generatedNickname = `${randomEmoji} ${randomPrefix} ${name} ${randomSuffix} ${randomEmoji}`;
-
-	if (asciiStyles[asciiStyle]) {
-		generatedNickname = asciiStyles[asciiStyle](generatedNickname);
-	}
-
-	setNickname(generatedNickname);
-	};
-
+	// Handles the form submission and triggers nickname generation
 	const handleSubmit = (event) => {
-		event.preventDefault();
-		generateNickname();
+		event.preventDefault(); // Prevent the default form submission behavior
+		generateNickname(); // Generate a new nickname when the form is submitted
 	};
 
-	return (
+return (
 		<div className="container py-5">
-		<form onSubmit={handleSubmit}>
-		  <h2 className="mb-3">Nickname generator</h2>
-		  <div className="form-group">
-			<label htmlFor="name">Name:</label>
-			<input className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-		  </div>
-		  <div className="form-check">
-			<input className="form-check-input" type="checkbox" id="useEmojis" checked={useEmojis} onChange={() => setUseEmojis(!useEmojis)} />
-			<label className="form-check-label" htmlFor="useEmojis">Use Emojis</label>
-		  </div>
-		  <div className="form-check">
-			<input className="form-check-input" type="checkbox" id="usePrefix" checked={usePrefix} onChange={() => setUsePrefix(!usePrefix)} />
-			<label className="form-check-label" htmlFor="usePrefix">Use Prefix</label>
-		  </div>
-		  <div className="form-check">
-			<input className="form-check-input" type="checkbox" id="useSuffix" checked={useSuffix} onChange={() => setUseSuffix(!useSuffix)} />
-			<label className="form-check-label" htmlFor="useSuffix">Use Suffix</label>
-		  </div>
-		  <div className="form-group">
-			<label htmlFor="asciiStyle">ASCII Style:</label>
-			<select className="form-select" id="asciiStyle" value={asciiStyle} onChange={(e) => setAsciiStyle(e.target.value)}>
-			  <option value="">None</option>
-			  <option value="uppercase">Uppercase</option>
-			  <option value="leet">Leet Style</option>
-			</select>
-		  </div>
-		  <button type="submit" className="btn btn-primary mt-3">Generate Nickname</button>
-		</form>
-		<h3 className="mt-3">Generated Nickname: {nickname}</h3>
-	  </div>
+			{/* Nickname Generator Form */}
+			<form onSubmit={handleSubmit}>
+				<h2 className="mb-3">Nickname generator</h2>
+				
+				{/* Input field for the name */}
+				<div className="form-group">
+					<label htmlFor="name">Name:</label>
+					<input 
+						className="form-control" 
+						id="name" 
+						value={name} 
+						onChange={(e) => setName(e.target.value)} 
+					/>
+				</div>
+				
+				{/* Checkbox to toggle the use of emojis */}
+				<div className="form-check">
+					<input 
+						className="form-check-input" 
+						type="checkbox" 
+						id="useEmojis" 
+						checked={useEmojis} 
+						onChange={() => setUseEmojis(!useEmojis)} 
+					/>
+					<label className="form-check-label" htmlFor="useEmojis">Use Emojis</label>
+				</div>
+				
+				{/* Checkbox to toggle the use of prefix */}
+				<div className="form-check">
+					<input 
+						className="form-check-input" 
+						type="checkbox" 
+						id="usePrefix" 
+						checked={usePrefix} 
+						onChange={() => setUsePrefix(!usePrefix)} 
+					/>
+					<label className="form-check-label" htmlFor="usePrefix">Use Prefix</label>
+				</div>
+				
+				{/* Checkbox to toggle the use of suffix */}
+				<div className="form-check">
+					<input 
+						className="form-check-input" 
+						type="checkbox" 
+						id="useSuffix" 
+						checked={useSuffix} 
+						onChange={() => setUseSuffix(!useSuffix)} 
+					/>
+					<label className="form-check-label" htmlFor="useSuffix">Use Suffix</label>
+				</div>
+				
+				{/* Dropdown to select ASCII style */}
+				<div className="form-group">
+					<label htmlFor="asciiStyle">ASCII Style:</label>
+					<select 
+						className="form-select" 
+						id="asciiStyle" 
+						value={asciiStyle} 
+						onChange={(e) => setAsciiStyle(e.target.value)}
+					>
+						<option value="">None</option>
+						<option value="uppercase">Uppercase</option>
+						<option value="leet">Leet Style</option>
+					</select>
+				</div>
+				
+				{/* Button to submit the form and generate the nickname */}
+				<button type="submit" className="btn btn-primary mt-3">Generate Nickname</button>
+			</form>
+			
+			{/* Display area for the generated nickname */}
+			<h3 className="mt-3">Generated Nickname: {nickname}</h3>
+		</div>
 	);
 };
 
