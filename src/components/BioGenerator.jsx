b02bb98bc10dc6fe0e56adad36c19419c83026e1
AlexYelisieiev <62658287+AlexYelisieiev@users.noreply.github.com>
2023-12-05 13:16:09 +0000

    Fix select

--- src/components/BioGenerator.jsx
+++ src/components/BioGenerator.jsx
@@ -1,132 +1,132 @@
 import React, { useState } from 'react';
 
 const BioGenerator = () => {
-	const [selectedTheme, setSelectedTheme] = useState('');
-	const [sections, setSections] = useState([
-		{ label: 'Profession', value: '' },
-		{ label: 'Hobbies', value: '' },
-		{ label: 'Catchphrase', value: '' },
-	]);
-	const [bio, setBio] = useState('');
+    const [selectedTheme, setSelectedTheme] = useState('');
+    const [sections, setSections] = useState([
+        { label: 'Profession', value: '' },
+        { label: 'Hobbies', value: '' },
+        { label: 'Catchphrase', value: '' },
+    ]);
+    const [bio, setBio] = useState('');
 
-	const updateSectionValue = (index, value) => {
-		const updatedSections = [...sections];
-		updatedSections[index].value = value;
-		setSections(updatedSections);
-	};
+    const updateSectionValue = (index, value) => {
+        const updatedSections = [...sections];
+        updatedSections[index].value = value;
+        setSections(updatedSections);
+    };
 
-	const updateSectionLabel = (index, label) => {
-		const updatedSections = [...sections];
-		updatedSections[index].label = label;
-		setSections(updatedSections);
-	};
+    const updateSectionLabel = (index, label) => {
+        const updatedSections = [...sections];
+        updatedSections[index].label = label;
+        setSections(updatedSections);
+    };
 
-	const addSection = (event) => {
-		event.preventDefault();
-		const newSection = { label: 'New Section', value: '' };
-		setSections([...sections, newSection]);
-	};
+    const addSection = (event) => {
+        event.preventDefault();
+        const newSection = { label: 'New Section', value: '' };
+        setSections([...sections, newSection]);
+    };
 
-	const removeSection = (event, index) => {
-		event.preventDefault();
-		const updatedSections = [...sections];
-		updatedSections.splice(index, 1);
-		setSections(updatedSections);
-	};
+    const removeSection = (event, index) => {
+        event.preventDefault();
+        const updatedSections = [...sections];
+        updatedSections.splice(index, 1);
+        setSections(updatedSections);
+    };
 
-	const generateBio = (event) => {
-		// Define different bio styles based on selected theme
-		const bioThemes = {
-			default: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n'),
-			sections: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n==========\n'),
-			dashes: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n----------\n'),
-			dots: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n..........\n'),
-			jungle: () => `🌿 ${sections.map((section) => `${section.label}: ${section.value}`).join(' 🌿\n🌿 ')} 🌿`,
-			beach: () => `🏖️ ${sections.map((section) => `${section.label}: ${section.value}`).join(' 🏝️\n🏝️ ')} 🏖️`,
-			stars: () => `✨ ${sections.map((section) => `${section.label}: ${section.value}`).join(' ✨\n✨ ')} ✨`,
-			// Add more themes as needed
-		};
+    const generateBio = (event) => {
+        // Define different bio styles based on selected theme
+        const bioThemes = {
+            default: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n'),
+            sections: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n==========\n'),
+            dashes: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n----------\n'),
+            dots: () => sections.map((section) => `${section.label}: ${section.value}`).join('\n..........\n'),
+            jungle: () => `🌿 ${sections.map((section) => `${section.label}: ${section.value}`).join(' 🌿\n🌿 ')} 🌿`,
+            beach: () => `🏖️ ${sections.map((section) => `${section.label}: ${section.value}`).join(' 🏝️\n🏝️ ')} 🏖️`,
+            stars: () => `✨ ${sections.map((section) => `${section.label}: ${section.value}`).join(' ✨\n✨ ')} ✨`,
+            // Add more themes as needed
+        };
 
-		// Set the generated bio using setBio
-		setBio(bioThemes[selectedTheme]());
-	};
+        // Set the generated bio using setBio
+        setBio(bioThemes[selectedTheme]());
+    };
 
-	return (
-		<div className='px-3 py-2'>
-			<h2 className='mb-2' style={{ paddingTop: "7.5vmin" }}>Bio generator</h2>
-			<form>
-				<div className="row mb-2">
-					<label className='col-sm-3 col-form-label' htmlFor="theme">Select Theme:</label>
-					<div className='col-sm-9'>
-						<select
-						  className='form-selec'
-						  id="theme"
-						  value={selectedTheme}
-						  defaultValue={'default'}
-						  onChange={(e) => setSelectedTheme(e.target.value)}
-						>
-						  <option value="default">Default</option>
-						  <option value="sections">Sections</option>
-						  <option value="dashes">Dashes</option>
-						  <option value="dots">Dots</option>
-						  <option value="jungle">Jungle Style</option>
-						  <option value="beach">Beach Style</option>
-						  <option value="stars">Stars Style</option>
-						  {/* Add more themes as needed */}
-						</select>
-					</div>
-				</div>
+    return (
+        <div className='px-3 py-2'>
+            <h2 className='mb-2' style={{ paddingTop: "7.5vmin" }}>Bio generator</h2>
+            <form>
+                <div className="row mb-2">
+                    <label className='col-sm-3 col-form-label' htmlFor="theme">Select Theme:</label>
+                    <div className='col-sm-9'>
+                        <select
+                            className='form-select'
+                            id="theme"
+                            value={selectedTheme}
+                            defaultValue={'default'}
+                            onChange={(e) => setSelectedTheme(e.target.value)}
+                        >
+                            <option value="default">Default</option>
+                            <option value="sections">Sections</option>
+                            <option value="dashes">Dashes</option>
+                            <option value="dots">Dots</option>
+                            <option value="jungle">Jungle Style</option>
+                            <option value="beach">Beach Style</option>
+                            <option value="stars">Stars Style</option>
+                            {/* Add more themes as needed */}
+                        </select>
+                    </div>
+                </div>
 
-				{sections.map((section, index) => (
-					<div>
-						<div key={index} className="row mb-2">
-							<label className='col-sm-4 col-form-label' htmlFor={`section-title-${index}`}>Section Title:</label>
-							<div className='col-sm-8'>
-								<input
-									className='form-control'
-									type="text"
-									id={`section-title-${index}`}
-									value={section.label}
-									onChange={(e) => updateSectionLabel(index, e.target.value)}
-								/>
-							</div>
-						</div>
-						<div className='row mb-2'>
-							<label className='col-sm-4 col-form-label' htmlFor={`section-${index}`}>Section Content:</label>
-							<div className='col-sm-8'>
-								<input
-									className='form-control'
-									type="text"
-									id={`section-${index}`}
-									value={section.value}
-									onChange={(e) => updateSectionValue(index, e.target.value)}
-								/>
-							</div>
-							<div>
-								<button className='btn btn-primary my-2' onClick={event => removeSection(event, index)}>
-									Remove
-								</button>
-							</div>
-						</div>
-					</div>
-				))}
+                {sections.map((section, index) => (
+                    <div>
+                        <div key={index} className="row mb-2">
+                            <label className='col-sm-4 col-form-label' htmlFor={`section-title-${index}`}>Section Title:</label>
+                            <div className='col-sm-8'>
+                                <input
+                                    className='form-control'
+                                    type="text"
+                                    id={`section-title-${index}`}
+                                    value={section.label}
+                                    onChange={(e) => updateSectionLabel(index, e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className='row mb-2'>
+                            <label className='col-sm-4 col-form-label' htmlFor={`section-${index}`}>Section Content:</label>
+                            <div className='col-sm-8'>
+                                <input
+                                    className='form-control'
+                                    type="text"
+                                    id={`section-${index}`}
+                                    value={section.value}
+                                    onChange={(e) => updateSectionValue(index, e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <button className='btn btn-primary my-2' onClick={event => removeSection(event, index)}>
+                                    Remove
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
 
-				<div className='mt-2 mb-3'>
-					<button className='btn btn-primary me-2' onClick={addSection}>
-						Add Section
-					</button>
-					<button className='btn btn-primary' onClick={event => { event.preventDefault(); generateBio(event) }}>
-						Generate Bio
-					</button>
-				</div>
+                <div className='mt-2 mb-3'>
+                    <button className='btn btn-primary me-2' onClick={addSection}>
+                        Add Section
+                    </button>
+                    <button className='btn btn-primary' onClick={event => { event.preventDefault(); generateBio(event) }}>
+                        Generate Bio
+                    </button>
+                </div>
 
-				<div className='row'>
-					<span className='text-bold'>Generated Bio:</span>
-					<pre>{bio}</pre>
-				</div>
-			</form>
-		</div>
-	);
+                <div className='row'>
+                    <span className='text-bold'>Generated Bio:</span>
+                    <pre>{bio}</pre>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default BioGenerator;
