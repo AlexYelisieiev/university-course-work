3dcc14d60774fa1e937edf0aee7e7640c6279c2c
Alex <taus.anarchy@gmail.com>
2023-12-05 16:22:23 +0200

    Add comments

--- src/components/AchievementSection.jsx
+++ src/components/AchievementSection.jsx
@@ -52,54 +52,60 @@ const AchievementSection = () => {
 
     // The JSX for the component, laying out the form and the list of profiles.
     return (
-        <div>
-            <h2 className="mb-2" style={{ paddingTop: '7.5vmin' }}>
-                Social Profiles
-            </h2>
-            {fields.map((field) => (
-                <div className="row mb-2" key={field.key}>
-                    <label className="col-sm-2 col-form-label" htmlFor={field.key}>
-                        {field.name}:
-                    </label>
-                    <div className="col-sm-8">
-                        <input
-                            className="form-control"
-                            type="text"
-                            id={field.key}
-                            placeholder={`Enter ${field.name}`}
-                            value={field.value}
-                            onChange={(e) => handleInputChange(field.key, e.target.value)}
-                        />
-                    </div>
-                    <div className="col-sm-2">
-                        <button
-                            className="btn btn-danger"
-                            onClick={() => deleteField(field.key)}
-                        >
-                            Delete
-                        </button>
-                    </div>
+    <div>
+        {/* Title for the Social Profiles section */}
+        <h2 className="mb-2" style={{ paddingTop: '7.5vmin' }}>
+            Social Profiles
+        </h2>
+        {/* Map through all fields and render input for each */}
+        {fields.map((field) => (
+            <div className="row mb-2" key={field.key}>
+                {/* Field label */}
+                <label className="col-sm-2 col-form-label" htmlFor={field.key}>
+                    {field.name}:
+                </label>
+                {/* Input for the field value */}
+                <div className="col-sm-8">
+                    <input
+                        className="form-control"
+                        type="text"
+                        id={field.key}
+                        placeholder={`Enter ${field.name}`}
+                        value={field.value}
+                        onChange={(e) => handleInputChange(field.key, e.target.value)}
+                    />
                 </div>
-            ))}
-            <div className="row mb-2">
-                <div className="col-sm-10 offset-sm-2">
+                {/* Delete button for the field */}
+                <div className="col-sm-2">
                     <button
-                        className="btn btn-success"
-                        onClick={() => {
-                            const fieldName = prompt('Enter social profile name:');
-                            if (fieldName) {
-                                addField(fieldName);
-                            }
-                        }}
+                        className="btn btn-danger"
+                        onClick={() => deleteField(field.key)}
                     >
-                        Add Profile
+                        Delete
                     </button>
                 </div>
             </div>
-            // Button to trigger the addition of a new profile using addProfile function
-            <button className="btn btn-primary mb-3" onClick={addProfile}>
-                Add Profiles
-            </button>
+        ))}
+        {/* Button to add new profile field */}
+        <div className="row mb-2">
+            <div className="col-sm-10 offset-sm-2">
+                <button
+                    className="btn btn-success"
+                    onClick={() => {
+                        const fieldName = prompt('Enter social profile name:');
+                        if (fieldName) {
+                            addField(fieldName);
+                        }
+                    }}
+                >
+                    Add Profile
+                </button>
+            </div>
+        </div>
+        {/* Button to add a new profile to the list */}
+        <button className="btn btn-primary mb-3" onClick={addProfile}>
+            Add Profiles
+        </button>
 
             <div>
                 {/* Iterate over each profile in socialProfiles and display them */}
